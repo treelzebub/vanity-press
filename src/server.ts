7e85a3519d4607079a5060ec76289953702691e3
treelzebub <treelzebub@gmail.com>
2022-12-03 01:00:15 -0800

    parser is functional, but url fetch times out a lot

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,17 @@
 import express from 'express';
 import moment from 'moment';
 import { Zine } from './model/Zine';
+import { parser } from './parser.js';
 
 const app: express.Application = express();
 const PORT = parseInt(process.env.PORT || "1337");
 
 // Body Parser Middleware
 app.use(express.json());
+app.use((req, res, next) => {
+  console.log(req.body);
+  next()
+})
 
 app.get('/', (request, response) => {
   response.send(`What is up, my dude. It is now ${moment().format()}.`);
@@ -15,12 +20,21 @@ app.get('/', (request, response) => {
 app.post('/generate-zine', (request, response) => {
   var success = false;
   const body = request.body;
-  const zine = new Zine(body.email, body.title, body.contents, body.config);
-  const titles = zine.contents.map(it => it.title).join(", ");
 
   // TODO validation
 
-  response.send(`Received ${zine.title} for ${zine.email}\nwith contents: ${titles}`)
+  // response.send(parser(body)[0].title)
+  parser(body).then(r => {
+    try {
+      // const obj = JSON.parse(r);
+      // console.log(obj.excerpt);
+      console.log(r.excerpt);
+      response.status(200).send();
+    } catch (e) {
+      response.status(500).send();
+      console.log(e);
+    }
+  })
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
