882d705c334ed00bce1f64a4ccbd7be79c0cb610
treelzebub <treelzebub@gmail.com>
2022-12-04 16:22:01 -0800

    removing DKIM made email to kindle work lol

--- src/email/emailer.ts
+++ src/email/emailer.ts
@@ -18,17 +18,13 @@ const email = async (filename: string, email: string) => {
     to: email,
     subject: 'Your ebook is ready!',
     text: 'Please enjoy your bespoke, artisinal collection of articles!',
-    html: '<div>Please enjoy your bespoke, artisinal collection of articles!</div>',
     attachments:[
       {
-        filename
+        filename,
+        path: './' + filename,
+        contentType: 'application/epub+zip'
       }
-    ],
-    dkim: {
-      domainName: process.env.DKIM_DOMAIN!,
-      keySelector: process.env.DKIM_SELECTOR!,
-      privateKey: process.env.DKIM_KEY!
-    }
+    ]
   }).then(info => {
     console.log(`Email sent. Accepted: ${info.accepted}\nRejected: ${info.rejected}\nPending: ${info.pending || 'none'}.\nResponse: ${info.response}.\n\n`)
   }).catch(err => console.error(err));
